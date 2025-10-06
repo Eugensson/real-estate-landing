5429eb7e37ae30a9f5729a92684e9f7a51c68230
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-10-06 11:46:48 +0300

    Initialized project, added assets and styles, installed npm packages

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Find Your Dream Home with HomeLand | Real Estate for Sale & Rent",
+    template: "%s | HomeLand Real Estate",
+  },
+  description:
+    "Discover your perfect property with HomeLand — trusted real estate experts offering homes, apartments, and commercial spaces for sale and rent. Explore listings, compare prices, and find your new home today.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
